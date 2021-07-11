2d3ef1bee8b84d01107105a4d0cfb8d743909107
Martin Elsässer <develop@melsaesser.de>
2021-07-11 09:53:18 +0200

    Parameter "--fixmissing" bei apt-get hinzugefügt

--- rootfs/docker/init/aptInstall.sh
+++ rootfs/docker/init/aptInstall.sh
@@ -3,7 +3,7 @@
 # die Übergebenen Pakete per apt-get installieren und das Image wieder aufräumen
 
 packages="$*";
-apt-get update \
-	&& apt-get install -y --no-install-recommends ${packages} \
+apt-get update --fix-missing -y \
+	&& apt-get install --fix-missing -y --no-install-recommends ${packages} \
 	&& apt-get clean \
 	&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /var/log/*
